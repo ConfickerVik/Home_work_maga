d04c42782e178eb5ceb5cd1d9d12770d42708db6
confickervik <VPuRS*lodx854321>
2021-10-06 20:29:19 +0800

    create client

---
+++ laba4/client.py
@@ -0,0 +1,59 @@
+import socket
+import os
+import sys
+
+if __name__ == "__main__":
+    print("Здравствуй, введи команду для работы с сервером!")
+    print("<genuser> - Сгенерировать пользователей и их омс!")
+    print("<genres> - Сгенерировать результаты тестов!")
+    print("<query> - Отправить запрос в бд!")
+    print("<ex> - Выход из программы!")
+    print()
+
+    # настройки подключения к серверу
+    # sock = socket.socket()
+    # sock.connect(('localhost', 9090))
+    while True:
+        query_json = {}
+        comand = str(input("Введите команду: "))
+        if comand == 'ex':
+            # sock.send(message.encode())
+            print('Сессия завершена!')
+            # sock.close()
+            sys.exit()
+        elif comand == "genuser":
+            fname = [str(input(f"Введите {x}-ое имя: ")) for x in range(3)]
+            mname = [str(input(f"Введите {x}-ую фамилию: ")) for x in range(3)]
+            sname = [str(input(f"Введите {x}-ое отчество: ")) for x in range(3)]
+            query_json["comand"] = "gen_user"
+            query_json["data"] = [fname, mname, sname]
+            # Отправляем на серв, там сгенерируем номера омс и даты рождения
+            # sock.send(query_json.encode())
+            # Печатаем ответ сервера
+            # print(sock.recv(1024).decode())
+        elif comand == "genres":
+            query_json["comand"] = "gen_res"
+            # Отправляем на серв, там сгенерируем номера омс и даты рождения
+            # sock.send(query_json.encode())
+            # Печатаем ответ сервера
+            # print(sock.recv(1024).decode)
+        elif comand == "query":
+            print("Вы попали в мир запросов) Вам необходимо ввести некоторые данные: ")
+            fname = str(input("Введите имя пациента: "))
+            mname = str(input("Введите отчество пациента: "))
+            sname = str(input("Введите фамилию пациента: "))
+            oms = input("Введите номер омс пациента: ")
+            query_json["comand"] = "query_db"
+            query_json["fname"] = fname
+            query_json["mname"] = mname
+            query_json["sname"] = sname
+            query_json["oms"] = oms
+            # Отправляем на серв, там сгенерируем номера омс и даты рождения
+            # sock.send(query_json.encode())
+            # Печатаем ответ сервера
+            # print(sock.recv(1024).decode)
+        else:
+            # sock.send(message.encode())
+            # Вывод результата обработки сервером
+            # print(sock.recv(1024).decode())
+            print("LOL!")
